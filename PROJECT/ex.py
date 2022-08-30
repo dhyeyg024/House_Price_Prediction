6f83b5071a5c0babc1f15314411ad9a1f681b68d
dhyeyg024 <98167340+dhyeyg024@users.noreply.github.com>
2022-08-30 15:33:30 +0530

    Add files via upload

---
+++ PROJECT/ex.py
@@ -0,0 +1,23 @@
+# with open('g.txt','r') as f:
+#     count = 0
+#     text = f.read()
+#     for i in text:
+#         if i.isupper():
+#             count+=1
+#     print(count)
+
+# print(lambda x: count=+1 if x.isupper()  in f with open('g.txt') as f)
+# len([x for t in open('g.txt') for x in t if x.isupper()])
+# def check(file):
+#     return sum(map(x.isupper, file))
+#
+#
+# print("Capital Letters: ", sum(1 for x in open('g.txt','r') as file in file if x.isupper()))
+
+# sum(x.isupper() if open('g.txt') as f and x in )
+# print(sum(list(lambda c: c.isupper(), open('g.txt'))))
+
+str = int(input('Enter number:'))
+if str in range(-2147483648, 2147483648);
+
+
